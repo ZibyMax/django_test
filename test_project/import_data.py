92054d52eac2e489e45b011a0de0b78192110e60
ZibyMax <max@rekstar-nn.ru>
2019-07-18 17:04:58 +0300

    состыковал!

--- test_project/import_data.py
+++ test_project/import_data.py
@@ -1,27 +1,33 @@
 import os
+import sys
 import yaml
 from pprint import pprint
+import django
 
-BASE_DIR = os.path.join(os.path.dirname((os.path.abspath(__file__))), 'data')
+
+BASE_DIR = os.path.join(os.path.dirname((os.path.abspath(__file__))))
 
 
 def read_yaml(filename):
     try:
         with open(filename, encoding='utf-8') as f:
             data = yaml.load(f, Loader=yaml.SafeLoader)
-            #
-
-            pprint(data.keys())
-
-            #
     except Exception as e:
         print(f'ERROR!\n{e}')
+    return data
 
 
 def main():
-    file_list = os.listdir(BASE_DIR)
+    sys.path.append(os.path.join(BASE_DIR, 'test_project'))
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+    django.setup()
+    from test_app.models import Product
+
+    print(Product.objects.all())
+
+    file_list = os.listdir(os.path.join(BASE_DIR, 'data'))
     for filename in file_list:
-        read_yaml(os.path.join(BASE_DIR, filename))
+        data = read_yaml(os.path.join(BASE_DIR, 'data', filename))
 
 
 if __name__ == '__main__':
